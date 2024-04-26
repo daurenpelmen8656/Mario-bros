699425b226884bcae9af891d2d703d6f7ebd55f7
daurenpelmen8656 <155034944+daurenpelmen8656@users.noreply.github.com>
2024-04-26 16:59:24 +0500

    Animation, movement, map physic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]private float speed;
+    [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+        //Grab references for rigidbody and animator from object 
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
+
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if(Input.GetKey(KeyCode.Space) )
+        // Flip player when moving left-right
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(4, transform.localScale.y, transform.localScale.z);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-4, transform.localScale.y, transform.localScale.z);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //set animator parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
         {
-            body.velocity = new Vector2(body.velocity.x, speed);
+            grounded = true;
         }
     }
 }
+
