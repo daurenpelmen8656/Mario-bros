47b0de254fb3c5733d15d8ff1b797b23f8ec7978
daurenpelmen8656 <155034944+daurenpelmen8656@users.noreply.github.com>
2024-05-01 17:08:11 +0500

    Health bar

--- Assets/Scripts/HpBar.cs
+++ Assets/Scripts/HpBar.cs
@@ -3,31 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class HpBar : MonoBehaviour
 {
+
     private float HP = 100f;
     public Image Bar;
 
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
+    // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Snake")
-        {
-            HP -= 2;
-            Bar.fillAmount = HP/100;
-        }
-        if (collision.gameObject.tag == "Goblin")
+        if (collision.gameObject.tag == "Enemy")
         {
             HP -= 5;
             Bar.fillAmount = HP / 100;
         }
     }
 }
+
+
