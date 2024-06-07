437f20c2cd35f72a82050b944886e4a2c505cc48
daurenpelmen8656 <155034944+daurenpelmen8656@users.noreply.github.com>
2024-06-07 15:01:30 +0500

    камера границы каркас певрого уровня

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,74 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Enemy : MonoBehaviour
-{
-    public float speed;
-    public Vector3[] positions;
-    private int currentTarget;
-    private bool isFacingRight = true;
-    private SpriteRenderer spriteRenderer;
-
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    void Update()
-    {
-        if (currentTarget < positions.Length)
-        {
-            MoveToNextPosition();
-        }
-        else
-        {
-            currentTarget = 0;
-        }
-    }
-
-    private void MoveToNextPosition()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, positions[currentTarget], speed * Time.deltaTime);
-
-        if (transform.position == positions[currentTarget])
-        {
-            currentTarget++;
-        }
-
-        if (transform.position.x > positions[currentTarget].x && isFacingRight)
-        {
-            FlipSprite();
-        }
-        else if (transform.position.x < positions[currentTarget].x && !isFacingRight)
-        {
-            FlipSprite();
-        }
-    }
-
-    private void FlipSprite()
-    {
-        isFacingRight = !isFacingRight;
-        Vector3 newScale = transform.localScale;
-        newScale.x *= -1;
-        transform.localScale = newScale;
-    }
-
-    public int damageAmount = 10;
-    public delegate void PlayerDamaged(int damage);
-    public static event PlayerDamaged OnPlayerDamaged;
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            OnPlayerDamaged?.Invoke(damageAmount);
-        }
-    }
-}
-
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -88,6 +17,8 @@ public class Snake : MonoBehaviour
 
     void Update()
     {
+        if (positions.Length == 0) return; // Проверка, что массив позиций не пуст
+
         if (currentTarget < positions.Length)
         {
             MoveToNextPosition(); // Перемещаем змею к следующей позиции
@@ -100,6 +31,8 @@ public class Snake : MonoBehaviour
 
     private void MoveToNextPosition()
     {
+        if (currentTarget >= positions.Length) return; // Проверка, что текущий индекс в пределах массива
+
         // Перемещаем змею к текущей цели
         transform.position = Vector3.MoveTowards(transform.position, positions[currentTarget], speed * Time.deltaTime);
 
@@ -109,13 +42,16 @@ public class Snake : MonoBehaviour
         }
 
         // Проверяем направление движения и переворачиваем спрайт, если нужно
-        if (transform.position.x > positions[currentTarget].x && isFacingRight)
+        if (currentTarget < positions.Length)
         {
-            FlipSprite();
-        }
-        else if (transform.position.x < positions[currentTarget].x && !isFacingRight)
-        {
-            FlipSprite();
+            if (transform.position.x > positions[currentTarget].x && isFacingRight)
+            {
+                FlipSprite();
+            }
+            else if (transform.position.x < positions[currentTarget].x && !isFacingRight)
+            {
+                FlipSprite();
+            }
         }
     }
 
@@ -135,5 +71,3 @@ public class Snake : MonoBehaviour
         }
     }
 }
-
-
